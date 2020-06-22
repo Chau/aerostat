7045ce3a4a447ddb6c712c0447188a73a7a1c7a1
Vera <etavera@yandex.ru>
2020-06-22 22:22:16 +0800

    Walk over file system and parse.

--- parser/aerostat.py
+++ parser/aerostat.py
@@ -1,4 +1,5 @@
 import os
+import glob
 import json
 import datetime
 
@@ -7,7 +8,24 @@ from lxml.html import fromstring, tostring, HTMLParser
 from lxml.etree import ParserError
 
 
+class AerostatFolders():
 
+    def __init__(self, folder_path=None):
+        if not folder_path:
+            self.folder_path = '/home/vera/work/aerostat/AEROSTATICA/aerostatica.ru/'
+        else:
+            self.folder_path = folder_path
+
+    def get_all_files(self):
+        return glob.glob('{}/20??/??/??/*/index.html'.format(self.folder_path))
+
+    def parse_files(self):
+        res = []
+        for file in self.get_all_files():
+            print(file)
+            page = ParsePage(file)
+            res.append(page.parse())
+        return res
 
 
 class ParsePage():
@@ -40,7 +58,6 @@ class ParsePage():
         post = self.soup.find_class('post-content')[0]
         content = post.find_class("main-content-wrap")[0]
         res = []
-        # import pdb;pdb.set_trace()
         l = {'field_text': ''}
         for child in content.getchildren():
             if child.tag == 'p':
@@ -48,7 +65,11 @@ class ParsePage():
             elif child.tag == 'ul':
                 res.append(l)
                 # l = {}
-                l = {'field_name': child.findall(".//h4")[0].text_content()}
+                try:
+                    l = {'field_name': child.findall(".//h4")[0].text_content()}
+                except IndexError as e:
+                    print(str(e))
+                    pass
         res.append(l)
         return res
 
